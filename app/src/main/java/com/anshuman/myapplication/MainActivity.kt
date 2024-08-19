7c834aa799c6d0171c4b1a17ac2cfe49da2a33f4
Anshuman behera <beheraanshuman9@gmail.com>
2024-08-19 10:02:51 +0530

    test

--- app/src/main/java/com/anshuman/myapplication/MainActivity.kt
+++ app/src/main/java/com/anshuman/myapplication/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+            // update the title bar
+
     }
 }
