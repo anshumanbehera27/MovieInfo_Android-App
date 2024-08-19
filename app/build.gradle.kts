f66a261ee2c32ff7d116e4d69710d8c8e64c78ed
Anshuman behera <beheraanshuman9@gmail.com>
2024-08-19 10:43:00 +0530

    done with navigationView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,5 +52,35 @@ dependencies {
     // add the dependency for Lottiefiles
     implementation (libs.lottie)
 
+    // page navigation dependecy
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+
+    // add the dependecy for viewpager
+    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+
+    // add the depedency for floting navagating Bar
+    implementation ("com.github.ismaeldivita:chip-navigation-bar:1.4.0")
+    implementation("com.google.android.material:material:1.9.0")
+
+
+    // add for the blurView
+    implementation ("com.github.Dimezis:BlurView:version-2.0.5")
+
+    // json dependecy
+    implementation ("com.google.code.gson:gson:2.11.0")
+
+    // Glide is use to convert the one url to image
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    // retrofit dependency
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // for api request
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
 
 }
--- app/src/main/java/com/anshuman/myapplication/MainActivity.kt
+++ app/src/main/java/com/anshuman/myapplication/MainActivity.kt
@@ -2,12 +2,53 @@ package com.anshuman.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.anshuman.myapplication.Fragements.Fav_listFragment
+import com.anshuman.myapplication.Fragements.HomeFragment
+import com.anshuman.myapplication.Fragements.ProfileFragment
+import com.anshuman.myapplication.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            // update the title bar
+        /*
+      How to Define the Bottom Navigation
+          todo 1  Link with the Bottom navigation view
+          todo 2 - how the all the Fragemet will replace
+          todo 3 - set on item select listiner
+          todo 4 - define the defult fragement
+        */
+        val bottomNavigationView = binding.bottomNavigation
 
+        // load defult home one
+        replaceFragment(HomeFragment())
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> {
+
+                    true
+                }
+                R.id.favorites -> {
+                    replaceFragment(Fav_listFragment())
+                    true
+                }
+                R.id.profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
     }
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+    }
+
+
+
 }
